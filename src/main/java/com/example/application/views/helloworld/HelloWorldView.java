6b457bb2a9ef184d61e21bbcef82de3abb0f717e
sobek21 <mateuszsobkowicz211@gmail.com>
2020-11-06 00:26:40 +0100

    search api fixed

--- src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -4,9 +4,9 @@ import com.example.application.views.main.MainView;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
@@ -16,21 +16,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "hello-world", layout = MainView.class)
 @PageTitle("Hello World")
-@CssImport(value = "./styles/views/helloworld/google.css" , id = "google")
+@CssImport(value = "./styles/views/helloworld/google.css")
 @RouteAlias(value = "", layout = MainView.class)
 public class HelloWorldView extends VerticalLayout {
 
     Grid<VolumeInfo> grid = new Grid<>(VolumeInfo.class);
 
-    Label author = new Label("Author");
+
     Label name = new Label("Suggest Name");
     Label description = new Label("Description");
-    Label year = new Label("Publish year");
-    Label actors = new Label("Actors");
     TextField textField = new TextField("Szukaj ksiązki");
     Button button = new Button("Szukaj");
+
     H1 h1 = new H1("GOOGLE BOOKS SEARCH API");
 
+    Grid<VolumeInfo> bookGrid = new Grid<>(VolumeInfo.class);
+
+    VerticalLayout verticalLayout = new VerticalLayout();
+    HorizontalLayout horizontalLayout = new HorizontalLayout();
 
 
     private BookSearchService bookSearchService;
@@ -38,27 +41,37 @@ public class HelloWorldView extends VerticalLayout {
     @Autowired
     public HelloWorldView(BookSearchService bookSearchService) {
         this.bookSearchService = bookSearchService;
+        addClassName("user-view");
+        bookGrid.setColumns("authors","publisher","publishedDate","categories","pageCount");
+        bookGrid.setHeightByRows(true);
+        bookGrid.setVisible(false);
+        verticalLayout.add(bookGrid);
+        verticalLayout.setSizeFull();
 
-        addClassName("google");
+        horizontalLayout.add(textField,button);
 
-        h1.getElement().getStyle().set("font-size", "70px");
+
+setAlignSelf(Alignment.CENTER,h1);
+
+
+
 
         button.addClickListener(buttonClickEvent -> refresh());
 
-
-add(h1);
-        add(author, description, name,year,actors, textField, button);
-
+add(h1,horizontalLayout,name,verticalLayout);
+add(description);
 
 
     }
 
     public void refresh() {
-        author.setText(String.valueOf(bookSearchService.getBooks(textField.getValue()).getAuthors()));
-        description.setText(bookSearchService.getBooks(textField.getValue()).getDescription());
-        name.setText(bookSearchService.getBooks(textField.getValue()).getTitle());
-        year.setText(bookSearchService.getBooks(textField.getValue()).getPublishedDate());
-        actors.setText(String.valueOf(bookSearchService.getBooks(textField.getValue()).getCategories()));
-
+        if (textField.getValue()== null || textField.getValue()=="") {
+            bookGrid.setVisible(false);
+        }else {
+            bookGrid.setVisible(true);
+            bookGrid.setItems(bookSearchService.getBooks(textField.getValue()));
+            description.setText(String.valueOf(bookSearchService.getBooks(textField.getValue()).getDescription()));
+            name.setText(bookSearchService.getBooks(textField.getValue()).getTitle());
+        }
     }
 }
--- src/main/java/com/example/application/views/helloworld1/RestApi.java
+++ src/main/java/com/example/application/views/helloworld1/RestApi.java
@@ -3,12 +3,9 @@ package com.example.application.views.helloworld1;
 
 import com.example.application.views.empty.Book;
 import com.example.application.views.empty.BookService;
-import com.example.application.views.empty1.User;
 import com.example.application.views.empty1.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 public class RestApi {
@@ -23,7 +20,7 @@ public class RestApi {
     private UserService userService;
 
     public Book addToFav() {
-        User user = userService.findByUserName(principal.getName());
+     //   User user = userService.findByUserName(principal.getName());
 
         return null;
     }
